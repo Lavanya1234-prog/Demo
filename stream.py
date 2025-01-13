ab217f6e99077e836e06efb6349ad6d8f016537e
Lavanya1234-prog <lavanyakv118@gmail.com>
2025-01-13 15:09:05 +0530

    Update stream.py

--- stream.py
+++ stream.py
@@ -26,3 +26,17 @@ with st.status("Downloading data..."):
     time.sleep(1)
 
 st.button("Rerun")
+
+
+with st.status("Downloading data...", expanded=True) as status:
+    st.write("Searching for data...")
+    time.sleep(2)
+    st.write("Found URL.")
+    time.sleep(1)
+    st.write("Downloading data...")
+    time.sleep(1)
+    status.update(
+        label="Download complete!", state="complete", expanded=False
+    )
+
+st.button("Rerun")
