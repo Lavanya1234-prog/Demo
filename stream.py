5f9250bcfad43ec830767185a53c53efc5b52363
Lavanya1234-prog <lavanyakv118@gmail.com>
2025-01-13 15:08:18 +0530

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import numpy as np
+import time
+import streamlit as st
 
 st.header("This is my first streamlit App LETS ENJOY :blue[cool] :sunglasses:")
 
@@ -10,11 +12,17 @@ if prompt:
     st.write(f"User has sent the following prompt: {prompt}")
 
 
-with st.chat_message("user"):
-    st.write("Hello 👋")
-    st.line_chart(np.random.randn(30, 3))
-
-
 message = st.chat_message("assistant")
 message.write("Hello human")
 message.bar_chart(np.random.randn(30, 3))
+
+
+with st.status("Downloading data..."):
+    st.write("Searching for data...")
+    time.sleep(2)
+    st.write("Found URL.")
+    time.sleep(1)
+    st.write("Downloading data...")
+    time.sleep(1)
+
+st.button("Rerun")
