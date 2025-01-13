4c95c974a38d0d1c31611b16ea3964ffc3b40f23
Lavanya1234-prog <lavanyakv118@gmail.com>
2025-01-13 15:07:12 +0530

    Update stream.py

--- stream.py
+++ stream.py
@@ -13,3 +13,8 @@ if prompt:
 with st.chat_message("user"):
     st.write("Hello 👋")
     st.line_chart(np.random.randn(30, 3))
+
+
+message = st.chat_message("assistant")
+message.write("Hello human")
+message.bar_chart(np.random.randn(30, 3))
