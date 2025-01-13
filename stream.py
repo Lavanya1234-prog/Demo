794e9ea8640b9aea39d6795473da327af5e3d0e6
Lavanya1234-prog <lavanyakv118@gmail.com>
2025-01-13 15:04:13 +0530

    Update stream.py

--- stream.py
+++ stream.py
@@ -9,4 +9,8 @@ if prompt:
     st.write(f"User has sent the following prompt: {prompt}")
 
 
-
+with st.sidebar:
+    messages = st.container(height=300)
+    if prompt := st.chat_input("Say something"):
+        messages.chat_message("user").write(prompt)
+        messages.chat_message("assistant").write(f"Echo: {prompt}")
