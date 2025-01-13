91723bf0056e0604d3151e148006294138a01749
Lavanya1234-prog <lavanyakv118@gmail.com>
2025-01-13 15:05:28 +0530

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import numpy as np
 
 st.header("This is my first streamlit App LETS ENJOY :blue[cool] :sunglasses:")
 
@@ -9,8 +10,6 @@ if prompt:
     st.write(f"User has sent the following prompt: {prompt}")
 
 
-with st.sidebar:
-    messages = st.container(height=300)
-    if prompt := st.chat_input("Say something"):
-        messages.chat_message("user").write(prompt)
-        messages.chat_message("assistant").write(f"Echo: {prompt}")
+with st.chat_message("user"):
+    st.write("Hello 👋")
+    st.line_chart(np.random.randn(30, 3))
